06a5ef718500142b7b0185ebe14d6d25d5128b4e
Disketteman <109335133+disketteman@users.noreply.github.com>
2022-11-27 21:24:53 +0100

    Simplified javascript

--- DotnetRaylibWasm/main.js
+++ DotnetRaylibWasm/main.js
@@ -1,26 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
 import { dotnet } from './dotnet.js'
 
-const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+await dotnet
+    .withDebugging(-1)
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
 
-setModuleImports('main.js', {
-    window: {
-        location: {
-            href: () => globalThis.window.location.href
-        }
-    }
-});
+dotnet.instance.Module['canvas'] = (function () {
+    return document.getElementById('canvas');
+})();
 
-const exports = await getAssemblyExports(getConfig().mainAssemblyName);
 await dotnet.run();
-
-// TODO: is there a better way to setup canvas directly in dotnet.run or prior that? 
-dotnet.instance.Module['canvas'] = (function() { return document.getElementById('canvas'); })();
-
-// this calls our C# code using raylib 
-exports.DotnetRaylibWasm.RayTest.Start();
