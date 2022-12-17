2a332aa85b90e5e709ab4b5518794395fdea9016
disketteman <disketteman@users.noreply.github.com>
2022-12-17 20:00:18 +0000

    deploy: b936efefc2be39612df367f2c286423c9bb48c50

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ await dotnet
     .withApplicationArgumentsFromQuery()
     .create();
 
-dotnet.instance.Module['canvas'] = (function () {
-    return document.getElementById('canvas');
-})();
+dotnet.instance.Module['canvas'] = document.getElementById('canvas');
+
+// We're ready to dotnet.run, so let's remove the spinner
+const loading_div = document.getElementById('spinner');
+loading_div.remove();
 
 await dotnet.run();
